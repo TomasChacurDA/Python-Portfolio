8ae3f0a13971583a3dfaeb5dce77d948c1323511
TomasChacurDA <tomaschacurdev@gmail.com>
2024-04-11 11:37:46 -0300

    Second Commit

--- Clase04/costo_camion.py
+++ Clase04/costo_camion.py
@@ -8,20 +8,19 @@ import csv
        
 
 def costo_camion(nombre_archivo):
-        with open(nombre_archivo, 'rt') as f:
-            filas = csv.reader(f)
-            encabezados = next(filas)
-            costo_total = 0
-            for n_fila, fila in enumerate(filas, start=1):
-                record = dict(zip(encabezados, fila))
-                try:
-                    ncajones = int(record['cajones'])
-                    precio = float(record['precio'])
-                    costo_total += ncajones * precio
-                # Esto atrapa errores en los int() y float() de arriba.
-                except ValueError:
-                    print(f'Fila {n_fila}: No pude interpretar: {fila}')
-        return costo_total
+    with open(nombre_archivo, 'rt') as f:
+        filas = csv.reader(f)
+        encabezados = next(filas)
+        costo_total = 0
+        for n_fila, fila in enumerate(filas, start=1):
+            record = dict(zip(encabezados, fila))
+            try:
+                ncajones = int(record['cajones'])
+                precio = float(record['precio'])
+                costo_total += ncajones * precio
+            # Esto atrapa errores en los int() y float() de arriba.
+            except ValueError:
+                print(f'Fila {n_fila}: No pude interpretar: {fila}')
+    return costo_total
     
-        
-
+print(costo_camion('Data/camion.csv'))
